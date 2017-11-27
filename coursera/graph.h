3738d55c7828f8f5bf5bc6ef496e791fe3ed9c01
Raman Verma <footloose.raman@gmail.com>
2017-11-26 16:51:47 -0800

    Added basic functionality to graph header file, along with unit tests
    Added functionality to graph header:
    - Destrcutors for graph and vertex
    - Static methods to create random Directed or UnDirected Graphs
    - method to find if a graph is connected
    - Unit tests for all of the above

--- coursera/graph.h
+++ coursera/graph.h
@@ -1,26 +1,42 @@
 /* Graph library */
 
-#include<iostream>
-#include<vector>
-#include<forward_list>
+#pragma once
+
+#include <forward_list>
+#include <iostream>
+#include <memory>
+#include <queue>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
 /* data type for vertices of a graph */
-struct vertex {
+struct Vertex {
 public:
-  vertex() = delete;  // don't create a vertex with zeroed or random numbered *id* member
-  vertex(uint32_t id) : id_(id) { neighbors.clear(); }
+  Vertex() = delete;  // don't create a vertex with zeroed or random numbered *id* member
+  Vertex(uint32_t id) : id_(id) { neighbors_.clear(); }
+
+  ~Vertex() { neighbors_.clear(); }
+
+  /* get the vertex id*/
+  const uint32_t id() {return id_;}
+
+  /* returns const reference to neighbor list */
+  const forward_list<pair<uint32_t, uint32_t>> &
+  neighbors() { return neighbors_;}
 
   /* add a neighbor to the vertex, along with the cost/distance */
-  void addNeighbor(uint32_t n, int cost) { neighbors.emplace_front(n, cost); }
+  void addNeighbor(uint32_t n, uint32_t cost) { neighbors_.emplace_front(n, cost); }
+
 
   /* print the vertex and its neighbors */
-  friend ostream& operator<<(ostream& out, const struct vertex& v)
+  friend ostream& operator<<(ostream& out, const struct Vertex& v)
   {
     out << v.id_ << " : ";
-    for (auto neighbor: v.neighbors) {
-      out << "(" << neighbor.first << " " << neighbor.second << ") ";
+    for (auto neighbor: v.neighbors_) {
+      out << "(" << neighbor.first << ", cost: " << neighbor.second << ") ";
     }
     return out;
   }
@@ -28,35 +44,114 @@ private:
   // identifier for a vertex
   uint32_t id_;
   // list of neighbors to this vertex. list contains pairs of neighbor id and distance
-  forward_list<pair<uint32_t, int>> neighbors;
+  forward_list<pair<uint32_t, uint32_t>> neighbors_;
 };
-typedef struct vertex vertex;
+typedef struct Vertex Vertex;
 
+class Graph;
+typedef std::shared_ptr<Graph> GraphPtr;
 /* abstraction for a graph */
-class graph
+class Graph
 {
+private:
+	static const int32_t MAX_COST = 100;
 public:
-  graph() = delete;
-  graph(uint32_t numNodes)
-    : numNodes_(numNodes)
+  Graph() = delete;
+
+  Graph(uint32_t numNodes, bool directed)
+    : numNodes_(numNodes),
+	directed_(directed)
   {
     for (size_t i = 0; i < numNodes_; i++) {
       vertices_.emplace_back(i);
     }
   };
 
-  /* add an edge to the graph. returns false if either source or destnation vertex
-   * is out of bounds of the vertex ids used by the structure
-   */
-  bool addEdge(uint32_t src, uint32_t dest, int cost)
+  ~Graph()
   {
-    // both source and destination vertices must be part of the graph already
-    if (src >= vertices_.size() || dest >= vertices_.size()) {
-      return false;
-    }
-    vertices_.at(src).addNeighbor(dest, cost);
-    return true;
-  };
+	  vertices_.clear();
+  }
+
+	/* static method to create a random sized Graph. 
+	 * @param nodes [in] number of nodes for the graph
+	 * @param directed [in] bool value to indicate if the graph is directed, true for directed
+	 * @param density [in] density factor for graph. should be between 0 and 1
+	 * @return shared pointer to the graph
+	 */
+	static GraphPtr
+	generateRandomGraph(int numNodes = 10,
+						bool directed = false,
+						float density = 0.20)
+	{
+		srand(time(NULL));
+		if (directed) {
+			return randomDirectedGraph(numNodes, density);
+		} else {
+			return randomUnDirectedGraph(numNodes, density);
+		}
+	}
+
+  /* add an edge to the graph. slot at which a vertex is saved is denoted by
+   * the vertex's id. so, source and destination ids are supposed to be an
+   * unsigned int value bounded by the number of vertices
+   * @return false if either source or destnation vertex
+   * is out of bounds of the number of vertices used by the structure; true in
+   * case the edge is added successfully
+   */
+	bool
+	addEdge(const uint32_t src,
+			const uint32_t dest,
+			int cost)
+	{
+		if (src >= vertices_.size() || dest >= vertices_.size()) {
+		  return false;
+		}
+		vertices_.at(src).addNeighbor(dest, cost);
+		if (!directed_) {
+			vertices_.at(dest).addNeighbor(src, cost);
+		}
+		return true;
+	};
+
+  uint32_t numNodes() {
+  	return numNodes_;
+  }
+
+  const vector<Vertex>& vertices() {
+  	return vertices_;
+  }
+
+  /* determine if all the nodes are connected
+   * @return true if connected; false otherwise
+   */
+  bool is_connected() {
+	  if(numNodes_ <= 1) { return true; }
+
+	  bool visited[numNodes_];
+	  for (size_t i = 0; i < numNodes_; i++) {visited[i] = false;}
+	  queue<uint32_t> q;
+	  q.push(0);
+
+	  while(!q.empty()) {
+		auto id = q.front();
+		q.pop();
+		if (visited[id]) { // this vertex has already been visited
+			continue;
+		}
+		visited[id] = true;
+		auto neighbors = vertices_.at(id).neighbors();
+		for (auto it=neighbors.begin(); it != neighbors.end(); it++) {
+			if (!visited[it->first]) {
+				q.push(it->first);
+			}
+		}
+	  }
+	  uint32_t count = 0;
+	  for (size_t i = 0; i < numNodes_; i++) {
+		  if (visited[i]) { count++; }
+	  }
+	  return (count == numNodes_);
+  }
 
   /* prints the graph */
   void print() {
@@ -65,8 +160,73 @@ public:
     }
   }
 private:
+  /*
+   * generates an integer in the range (1 ... MAX_COST) depending on a chance/probability
+   * provided as input parameter
+   * @param probability [in] probablity that an integer within the range above will
+   * 	be created. It must be in the range 0.0 and 1.0
+   * @return an integer in the range (1...MAX_COST), OR 0
+   */
+  	static int32_t
+	flipCoin (float probability)
+	{
+		auto randomNum = (double)rand(); /*0.0 <= randomNum <= (double)RAND_MAX*/
+		if ((randomNum / RAND_MAX) <= probability) {
+			return ((rand() % MAX_COST) + 1);
+		}
+		return 0;
+	}
+	/* static method to create a random sized Directed Graph. 
+	 * @param nodes [in] number of nodes for the graph
+	 * @param density [in] density factor for graph. should be between 0 and 1
+	 * @return shared pointer to the graph
+	 */
+	static GraphPtr
+	randomDirectedGraph(int numNodes = 10,
+						float density = 0.20)
+	{
+		GraphPtr g = std::make_shared<Graph>(Graph(numNodes, true /*directed*/));
+		for (auto it = g->vertices_.begin(); it != g->vertices_.end(); it++)
+		{
+			for (auto it2 = g->vertices_.begin(); it2 != g->vertices_.end(); it2++)
+			{
+				if (it2->id() == it->id() /* no edge where src and dest are same*/) {
+					continue;
+				}
+				auto cost = flipCoin(density);
+				if (cost > 0) {
+					g->addEdge(it->id(), it2->id(), cost);
+				}
+			}
+		}
+		return g;
+	}
+	/* static method to create a random sized UnDirected Graph. 
+	 * @param nodes [in] number of nodes for the graph
+	 * @param density [in] density factor for graph. should be between 0 and 1
+	 * @return shared pointer to the graph
+	 */
+	static GraphPtr
+	randomUnDirectedGraph(int numNodes = 10,
+						float density = 0.20)
+	{
+		GraphPtr g = std::make_shared<Graph>(Graph(numNodes, false /*directed*/));
+		for (auto it = g->vertices_.begin(); it != g->vertices_.end(); it++)
+		{
+			for (auto it2 = std::next(it, 1); it2 != g->vertices_.end(); it2++)
+			{
+				auto cost = flipCoin(density);
+				if (cost > 0) {
+					g->addEdge(it->id(), it2->id(), cost);
+				}
+			}
+		}
+		return g;
+	}
   /* num of vertices in the graph */
   uint32_t numNodes_;
   /* vector containing vertices and their neighbor information */
-  vector<vertex> vertices_;
+  vector<Vertex> vertices_;
+  /* indicates if the graph is directed*/
+  bool directed_;
 };
--- coursera/test/graph.cc
+++ coursera/test/graph.cc
@@ -1,6 +1,8 @@
 /* unit tests for graph library */
 #include "graph.h"
 
+using namespace std;
+
 /* Create the following graph and print it
  * (o)---------(1)
  *   \        /
@@ -11,24 +13,61 @@
  */
 void test_graph_creation()
 {
-  graph g(5);
-  g.addEdge(0, 1, 2);
-  g.addEdge(0, 4, 2);
-  g.addEdge(1, 0, 2);
-  g.addEdge(1, 4, 2);
-  g.addEdge(2, 4, 2);
-  g.addEdge(2, 3, 2);
-  g.addEdge(3, 2, 2);
-  g.addEdge(3, 4, 2);
-  g.addEdge(4, 0, 2);
-  g.addEdge(4, 1, 2);
-  g.addEdge(4, 2, 2);
-  g.addEdge(4, 3, 2);
-  g.print();
+	cout << "TEST BEGIN: " << __PRETTY_FUNCTION__ << endl;
+	Graph g(5, false);
+	g.addEdge(0, 1, 2);
+	g.addEdge(0, 4, 2);
+	g.addEdge(1, 0, 2);
+	g.addEdge(1, 4, 2);
+	g.addEdge(2, 4, 2);
+	g.addEdge(2, 3, 2);
+	g.addEdge(3, 2, 2);
+	g.addEdge(3, 4, 2);
+	g.addEdge(4, 0, 2);
+	g.addEdge(4, 1, 2);
+	g.addEdge(4, 2, 2);
+	g.addEdge(4, 3, 2);
+	g.print();
+}
+
+void
+test_random_directed_graph_creation()
+{
+	cout << "TEST BEGIN: " << __PRETTY_FUNCTION__ << endl;
+	// create random Directed graph with 5 nodes and edge density of 0.25
+	auto g = Graph::generateRandomGraph(5, true, 0.25);
+	g->print();
+}
+
+void
+test_random_undirected_graph_creation()
+{
+	cout << "TEST BEGIN: " << __PRETTY_FUNCTION__ << endl;
+	// create random UnDirected graph with 5 nodes and edge density of 0.25
+	auto g = Graph::generateRandomGraph(5, false, 0.25);
+	g->print();
+}
+
+void
+test_is_connected()
+{
+	cout << "TEST BEGIN: " << __PRETTY_FUNCTION__ << endl;
+	// test if a random raph is connected or not
+	auto g = Graph::generateRandomGraph(5, true, 0.25);
+	g->print();
+	cout << "Graph is" << (g->is_connected() ? " ":" not ") << "connected" << endl;
+	g.reset();
+	g = Graph::generateRandomGraph(5, false, 0.25);
+	g->print();
+	cout << "Graph is" << (g->is_connected() ? " ":" not ") << "connected" << endl;
+
 }
 
 int
 main() {
-  test_graph_creation();
+	test_graph_creation();
+	test_random_directed_graph_creation();
+	test_random_undirected_graph_creation();
+	test_is_connected();
 }
 
